3c5bdc55141cfa9e46195f5c99849a3cf6278d68
Nengha-John <nenghajm@gmail.com>
2023-02-19 23:20:36 +0300

    [UPDATE] Added migrations

--- sms_payment_gateway/settings.py
+++ sms_payment_gateway/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'payments',
+    'api'
 ]
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
